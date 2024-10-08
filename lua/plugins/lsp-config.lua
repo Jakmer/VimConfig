c8bffec8024f53700010b794bcc272cd42f9bf16
Jakmer <kubamerta13@gmail.com>
2024-10-08 19:59:07 +0200

    Add rust analyzer

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        ensure_installed = { "lua_ls", "clangd", "rust_analyzer" }
       })
     end
   },
@@ -17,7 +17,21 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+        root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git")
+      })
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+        on_attach = function(client, bufnr)
+        end
+      })
+
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
